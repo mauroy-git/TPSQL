26c8cddefe3b736583423a0fd94aaa43dc16211f
Ryan <122646678+Casanelli@users.noreply.github.com>
2025-12-10 13:14:53 +0100

    Add files via upload

---
+++ TP SQL/InsertIntoCust.sql
@@ -0,0 +1,11 @@
+INSERT INTO CustomerOrders(IdRestaurant,Total_amount,Customer_name)
+VALUES (1,'12.34','Jeff'),
+       (1,'32.5','Samuel'),
+       (3,'10','Mumus'),
+       (4,'15.10','Knight'),
+	   (5,'8.70','Marc'),
+	   (2,'33.3','Venuala'),
+	   (2,'52.35','Klan'),
+	   (3,'5.98','hardy'),
+	   (4,'7.77','Sandman'),
+       (5,'55.22','Lee');
---
+++ TP SQL/InsertIntoEmployees.sql
@@ -0,0 +1,13 @@
+INSERT INTO Employees(Firstname,Lastname,role,IdRestaurant,hire_date)
+VALUES ('Kouassi','Lemaitre','Chef cuisto',1,2025-01-19),
+       ('Verla','Arouche','Serveuse',1,2025-05-24),
+       ('moog','pelus','chef cuisto',3,2032-06-26),
+       ('vuges','temo','chef cuisto',2,2028-11-22),
+       ('salome','chieffe','serveur',4,2024-10-05),
+	   ('savares','stung','chef cuisto',4,2025-10-16),
+       ('Piscine','Louis','serveur',1,2026-10-05),
+	   ('Marcus','Smarty','Chef cuisto',5,2025-02-13),
+	   ('calum','teffare','serveuse',2,2029-04-21),
+	   ('sicky','sodome','serveur',5,2025-07-07),
+	   ('sexy','leane','serveuse',3,2031-01-10);
+       
